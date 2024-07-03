93e3f087ecdefa03128fdb2c37e6258b9e62107d
boyiechen945 <boyie.chen.family@gmail.com>
2024-07-03 10:17:24 -0500

    update

--- main_model.R
+++ main_model.R
@@ -1,3 +1,15 @@
+rm(list = ls())
+
+library(dotenv)
+load_dot_env()
+
+PROJECT_PATH <- Sys.getenv("PROJECT_PATH")
+
+# set up working directory for R
+setwd(PROJECT_PATH)
+
+# Set up env
+Sys.setenv(TZ='US/Pacific')
 
 source("./config.R")
 source("./rshinyapp/cleanData.R")
